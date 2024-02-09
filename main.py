d878a77ca198b943209383803f7fd010ac1ab82d
augustin <augustin7766@gmail.com>
2024-02-09 13:08:04 +0530

    Model_training

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.WineQualityPrediction_with_MLflow import logger
 from src.WineQualityPrediction_with_MLflow.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.WineQualityPrediction_with_MLflow.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.WineQualityPrediction_with_MLflow.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from src.WineQualityPrediction_with_MLflow.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 # we can import logger in either way
 # since we have written logging inside the constructor file of WineQualityPrediction_with_MLflow folder we don't have to mention src folder
@@ -35,13 +36,30 @@ except Exception as e:
 
 
 
-if __name__ == '__main__':
-     try:
-        logger.info(f">>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<")
-        obj = DataTransformationTrainingPipeline()
-        obj.main()
-        logger.info(f">>>>>>>>>>>stage {STAGE_NAME} completed <<<<<<<<<<<<<<<")
+STAGE_NAME =    'Data Transformation Stage'
 
-     except Exception as e:
-        logger.exception(e)
-        raise e
+try:
+     logger.info(f">>>>>>>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<")
+     data_ingestion = DataTransformationTrainingPipeline()
+     data_ingestion.main()
+     logger.info(f" >>>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<<<<<")
+
+except Exception as e:
+     logger.exception(e)
+     raise e
+
+
+STAGE_NAME = 'Model Trainer Stage'
+
+try:
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<")
+    data_ingestion = ModelTrainerTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<")
+
+except Exception as e:
+     logger.exception(e)
+     raise e
+
+
+
--- src/WineQualityPrediction_with_MLflow/entity/config_entity.py
+++ src/WineQualityPrediction_with_MLflow/entity/config_entity.py
@@ -24,3 +24,15 @@ class DataValidationConfig:
 class DataTransformationConfig:
     root_dir : Path
     data_path : Path
+
+
+@dataclass
+class ModelTrainerConfig:
+    root_dir : Path
+    train_data_path : Path
+    test_data_path : Path
+    model_name : str
+    # the below model parameters are need to be defined in params.yaml file
+    alpha : float
+    l1_ratio : float
+    target_column : str
